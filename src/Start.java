4a532cf11e2fe1b50b2f7234e5674f1149ade955
Rino <kurarinorino@ymail.ne.jp>
2025-05-26 11:06:26 +0900

    3章

--- src/Start.java
+++ src/Start.java
@@ -5,8 +5,11 @@ public class Start {
 		// TODO 自動生成されたメソッド・スタブ
 		//System.out.println("藤牧京介");
 		//MyFrame frame1 = new RectTest();
-		System.out.println("プログラムを実行します");
-		MyFrame frame1 = new RectTest();
+		//System.out.println("プログラムを実行します");
+		//MyFrame frame1 = new RectTest();
+		System.out.println("でぃびじょん");
+		//MyFrame frame1 = new OvalTest();
+		MyFrame frame1=new TextTest();
 	}
 
 }
