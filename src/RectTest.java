2f505dd32325758b3ce7b5ac48159469ad07a50b
Rino <kurarinorino@ymail.ne.jp>
2025-05-26 10:23:13 +0900

    確認問題３

--- src/RectTest.java
+++ src/RectTest.java
@@ -2,8 +2,23 @@
 public class RectTest extends MyFrame {
 
 	public void run() {
-		fillRect(30,50,200,100);
-
+		//fillRect(30,50,200,100);
+		setColor(255,0,0);
+		fillRect(50,50,100,100);
+		
+		setColor(0,255,0);
+		fillRect(160,50,100,100);
+		
+		setColor(0,0,255);
+		fillRect(50,160,100,100);
+		
+		setColor(0,255,255);
+		fillRect(160,160,100,100);
+		
+		setColor(0,0,255);
+		fillRect(270,160,100,100);
+		
+		
 	}
 
 }
--- src/Start.java
+++ src/Start.java
@@ -3,7 +3,9 @@ public class Start {
 
 	public static void main(String[] args) {
 		// TODO 自動生成されたメソッド・スタブ
-		System.out.println("藤牧京介");
+		//System.out.println("藤牧京介");
+		//MyFrame frame1 = new RectTest();
+		System.out.println("プログラムを実行します");
 		MyFrame frame1 = new RectTest();
 	}
 
