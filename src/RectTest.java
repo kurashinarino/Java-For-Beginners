47ca38d14b79de1b9e138b6abfae93a30d5592dc
Rino <kurarinorino@ymail.ne.jp>
2025-05-26 10:27:13 +0900

    白枠追加

--- src/RectTest.java
+++ src/RectTest.java
@@ -3,20 +3,23 @@ public class RectTest extends MyFrame {
 
 	public void run() {
 		//fillRect(30,50,200,100);
-		setColor(255,0,0);
+		setColor(255,255,0);
 		fillRect(50,50,100,100);
 		
-		setColor(0,255,0);
+		setColor(255,0,255);
 		fillRect(160,50,100,100);
 		
-		setColor(0,0,255);
+		setColor(0,255,255);
 		fillRect(50,160,100,100);
 		
-		setColor(0,255,255);
+		setColor(0,0,0);
 		fillRect(160,160,100,100);
 		
-		setColor(0,0,255);
+		setColor(128,128,128);
 		fillRect(270,160,100,100);
+		
+		setColor(255,255,255);
+		fillRect(295,185,50,50);
 		
 		
 	}
