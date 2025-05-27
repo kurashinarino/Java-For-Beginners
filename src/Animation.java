855b7139999d100985944deb4973574c830e1690
Rino <kurarinorino@ymail.ne.jp>
2025-05-27 11:47:24 +0900

    5.3.6

--- src/Animation.java
+++ src/Animation.java
@@ -4,6 +4,7 @@ public class Animation extends MyFrame{
 	public void run() {
 		int y=100;
 		int x=30;
+	while(true) {
 		while(x<=170) {
 			//画面を消す
 			clear();
@@ -13,13 +14,13 @@ public class Animation extends MyFrame{
 			x+=5;//xを増やす(yは変更しない)
 			sleep(0.1);
 		}
-		while(y<=200) {
+		while(x>=30) {
 			//画面を消す
 			clear();
 			//四角形を表示する
 			setColor(0,128,0);
 			fillRect(x,y,10,100);//xとyを用いて表示
-			y+=5;//yを増やす(xは変更しない)
+			x-=5;//yを増やす(xは変更しない)
 			sleep(0.1);
 			}
 			
@@ -27,5 +28,6 @@ public class Animation extends MyFrame{
 			
 		
 	}
+}
 
 
