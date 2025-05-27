084a86ef9fe93a3de27c6e9092ca260cf9764109
Rino <kurarinorino@ymail.ne.jp>
2025-05-27 11:12:39 +0900

    5.3.2

--- src/Animation.java
+++ src/Animation.java
@@ -2,23 +2,24 @@
 public class Animation extends MyFrame{
 
 	public void run() {
-		int x=30;
-		while(true) {
+		int x=170;//30から170に変更
+		while(x>=30) {//xが30以上の間
 			//画面を消す
 			clear();
 			//四角形を表示する
 			setColor(0,128,0);
-			fillOval(x,80,50,50);
+			fillRect(x,80,10,100);
 			//x+=20;
 			//sleep(1);
-			x+=5;
-			if(x>200) {//xの値が200より大きくなったら
-				break;//whileによる繰り返しをやめる
-			}
+			x-=5;//左に動かすのでxの値を減らす
+			//if(x>200) {//xの値が200より大きくなったら
+				//break;//whileによる繰り返しをやめる
 			sleep(0.1);
+			}
+			
 		}
 			
 		
 	}
 
-}
+
